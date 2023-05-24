f622c253d97f8be144b6c0c0a42a6cc7fffabe3c
Miro Cosic <mirocosic@gmail.com>
2023-05-24 19:58:19 +0200

    fiddling with desktop layout

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -4,20 +4,255 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Cojones Timeline
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Cojones Timeline</h1>
       </header>
+      <div className="content">
+        <h2>The beginning</h2>
+
+        <h3 className="year-wrapper">
+          2006
+        </h3>
+
+
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+        <h3 className="year-wrapper">
+          2007
+        </h3>
+
+
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+        <div className="entry-wrapper indent-right">
+          <div className="card-entry ">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+        
+        <div className="entry-wrapper indent-left">
+          <div className="card-entry">
+            <div className="entry-title">
+              First gig
+            </div>
+            <div className="entry-desc">
+              Lorem ipsum, lorem ipseum....
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+
     </div>
   );
 }
